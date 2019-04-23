7e73df37c0b7ee6963ea38d39b338c85d62a4d4f
Shanmugha Priya <shanmu9195@gmail.com>
2019-04-23 16:16:26 +0530

    emaillink localhost:3000/email paytmlink localhost:3000/pay-withpaytm?amount=1

--- server.js
+++ server.js
@@ -89,6 +89,49 @@ let transporter = nodeMailer.createTransport(smtpTransport({
  
     });
     
+    
+const cors = require("cors");
+const ejs = require("ejs");
+
+require("dotenv").config();
+
+
+
+const {initPayment, responsePayment} = require("./paytm/services/index");
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + "/views"));
+app.set("view engine", "ejs");
+
+app.get("/pay-withpaytm", (req, res) => {
+    initPayment(req.query.amount).then(
+        success => {
+            res.render("paytmRedirect.ejs", {
+                resultData: success,
+                paytmFinalUrl: process.env.PAYTM_FINAL_URL
+            });
+        },
+        error => {
+            res.send(error);
+        }
+    );
+});
+
+app.post("/paywithpaytmresponse", (req, res) => {
+    responsePayment(req.body).then(
+        success => {
+            res.render("response.ejs", {resultData: "true", responseData: success});
+        },
+        error => {
+            res.send(error);
+        }
+    );
+});
+
 
 
 // handle 404 error
